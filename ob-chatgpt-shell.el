ccb982ed4d8d70de1b3b64bd9fdeb7725d5b3f41
xenodium <me+gh@xenodium.com>
2023-04-06 09:29:47 +0100

    Fixes ob preface bug

--- ob-chatgpt-shell.el
+++ ob-chatgpt-shell.el
@@ -58,8 +58,9 @@ This function is called by `org-babel-execute-src-block'"
   (message "executing ChatGPT source code block")
   (if (map-elt params :preface)
       (chatgpt-shell-post-chatgpt-messages
-       (append preface `(((role . "user")
-                          (content . ,body))))
+       (vconcat (map-elt params :preface)
+                `(((role . "user")
+                   (content . ,body))))
        (map-elt params :version))
       (chatgpt-shell-post-chatgpt-prompt body (map-elt params :version))))
 
