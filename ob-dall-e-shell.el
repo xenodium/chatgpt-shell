6e59b1e8206072b7622d08820b7409f52e5ab946
xenodium <me+gh@xenodium.com>
2023-04-29 14:07:08 +0100

    Updates ob-dall-e-shell dependency version

--- ob-dall-e-shell.el
+++ ob-dall-e-shell.el
@@ -5,7 +5,7 @@
 ;; Author: Alvaro Ramirez
 ;; URL: https://github.com/xenodium/chatgpt-shell
 ;; Version: 0.17.1
-;; Package-Requires: ((emacs "27.1") (dall-e-shell "0.17.1"))
+;; Package-Requires: ((emacs "27.1") (dall-e-shell "0.18.1"))
 
 ;;; License:
 
