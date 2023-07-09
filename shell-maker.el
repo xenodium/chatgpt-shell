ac0362e5b2c19712131de0b1144842bcb47b3788
xenodium <me+gh@xenodium.com>
2023-07-09 17:34:32 +0100

    Bumping version

--- shell-maker.el
+++ shell-maker.el
@@ -4,7 +4,7 @@
 
 ;; Author: Alvaro Ramirez https://xenodium.com
 ;; URL: https://github.com/xenodium/chatgpt-shell
-;; Version: 0.40.1
+;; Version: 0.41.1
 ;; Package-Requires: ((emacs "27.1"))
 
 ;; This package is free software; you can redistribute it and/or modify
