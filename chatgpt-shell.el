592b210d5aa8be4357f2496e57509cffd5b9cc52
Vaarnan Drolia <vellvisher@users.noreply.github.com>
2023-11-02 18:09:55 +0530

    Update chatgpt-shell-model-versions to include 16K context for GPT-3.5 Turbo

--- chatgpt-shell.el
+++ chatgpt-shell.el
@@ -201,6 +201,8 @@ Can be used compile or run source block at point."
 (defcustom chatgpt-shell-model-versions
   '("gpt-3.5-turbo"
     "gpt-3.5-turbo-0613"
+    "gpt-3.5-turbo-16k"
+    "gpt-3.5-turbo-16k-0613"
     "gpt-4"
     "gpt-4-0613")
   "The list of ChatGPT OpenAI models to swap from.
