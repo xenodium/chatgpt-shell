3faae91bda96b1d757e4c7095fe7a05b75c7279a
xenodium <me+gh@xenodium.com>
2024-12-03 21:09:31 +0000

    Bump shell-maker version to fontify validation errors

--- chatgpt-shell.el
+++ chatgpt-shell.el
@@ -5,7 +5,7 @@
 ;; Author: Alvaro Ramirez https://xenodium.com
 ;; URL: https://github.com/xenodium/chatgpt-shell
 ;; Version: 2.5.5
-;; Package-Requires: ((emacs "28.1") (shell-maker "0.73.1"))
+;; Package-Requires: ((emacs "28.1") (shell-maker "0.74.1"))
 (defconst chatgpt-shell--version "2.5.5")
 
 ;; This package is free software; you can redistribute it and/or modify
