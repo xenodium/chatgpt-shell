d26534b601c916225f1cbdf3eca0b7ccae5d8d96
xenodium <me+gh@xenodium.com>
2024-12-05 16:24:00 +0000

    Bumping version

--- chatgpt-shell.el
+++ chatgpt-shell.el
@@ -4,9 +4,9 @@
 
 ;; Author: Alvaro Ramirez https://xenodium.com
 ;; URL: https://github.com/xenodium/chatgpt-shell
-;; Version: 2.7.1
+;; Version: 2.7.2
 ;; Package-Requires: ((emacs "28.1") (shell-maker "0.74.1"))
-(defconst chatgpt-shell--version "2.7.1")
+(defconst chatgpt-shell--version "2.7.2")
 
 ;; This package is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
