a9ed9eb5ec2d16510f91469a9fbddd1e08fd2be5
xenodium <me+gh@xenodium.com>
2024-12-06 13:45:34 +0000

    Bumping version for compose feature

--- chatgpt-shell.el
+++ chatgpt-shell.el
@@ -4,9 +4,9 @@
 
 ;; Author: Alvaro Ramirez https://xenodium.com
 ;; URL: https://github.com/xenodium/chatgpt-shell
-;; Version: 2.7.3
+;; Version: 2.8.1
 ;; Package-Requires: ((emacs "28.1") (shell-maker "0.74.1"))
-(defconst chatgpt-shell--version "2.7.3")
+(defconst chatgpt-shell--version "2.8.1")
 
 ;; This package is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
