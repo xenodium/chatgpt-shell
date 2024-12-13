4077031d1647436553b1e18a71875addd768103f
xenodium <me+gh@xenodium.com>
2024-12-13 23:10:01 +0100

    Updates commentary

--- chatgpt-shell.el
+++ chatgpt-shell.el
@@ -23,7 +23,16 @@
 
 ;;; Commentary:
 
-;; `chatgpt-shell' is a comint-based ChatGPT shell for Emacs.
+;; `chatgpt-shell' is a comint-based shell for multiple cloud or local
+;; LLM services (ChatGPT, Claude, Gemini, Kagi, Ollama, Perplexity).
+;;
+;; This package also provides integrations like the following (amongst others):
+;;
+;; M-x `chatgpt-shell-quick-insert'
+;; M-x `chatgpt-shell-proofread-region'
+;; M-x `chatgpt-shell-prompt-compose'
+;; M-x `chatgpt-shell-describe-image'
+;; M-x `chatgpt-shell-japanese-lookup'
 ;;
 ;; You must set `chatgpt-shell-openai-key' to your key before using.
 ;;
