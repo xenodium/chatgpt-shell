ae82a2c31abe57791bb6c4b1d0d9dac4a3c43067
David J. Rosenbaum <djr7c4@gmail.com>
2025-02-28 13:57:34 -0800

    Add DeepSeek reasoner
    The previous code only had DeepSeek Chat. This adds DeepSeek Reasoner (which
    currently points to DeepSeek R1). See
    https://api-docs.deepseek.com/quick_start/pricing.

--- chatgpt-shell-deepseek.el
+++ chatgpt-shell-deepseek.el
@@ -46,6 +46,14 @@ VALIDATE-COMMAND and OTHER-PARAMS for `chatgpt-shell-openai-make-model'."
 (defun chatgpt-shell-deepseek-models ()
   "Build a list of DeepSeek LLM models."
   (list (chatgpt-shell-deepseek-make-model
+         :version "deepseek-reasoner"
+         :short-version "reasoner"
+         :label "DeepSeek"
+         ;; See https://api-docs.deepseek.com/quick_start/token_usage
+         :token-width 3
+         ;; See https://api-docs.deepseek.com/quick_start/pricing.
+         :context-window 65536)
+        (chatgpt-shell-deepseek-make-model
          :version "deepseek-chat"
          :short-version "chat"
          :label "DeepSeek"
