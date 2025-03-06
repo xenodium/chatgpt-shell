8ca5cf0072fbe8a56a8c6c5ed1c86a4e87c80caf
David J. Rosenbaum <djr7c4@gmail.com>
2025-03-05 23:27:07 -0800

    Add o1 to OpenAI models now that it is available to lower tier users

--- chatgpt-shell-openai.el
+++ chatgpt-shell-openai.el
@@ -87,6 +87,12 @@ HANDLER, FILTER and OTHER-PARAMS."
                  (format "Model \"%s\" does not support system prompts. Please unset via \"M-x chatgpt-shell-swap-system-prompt\" by selecting None."
                          (map-elt model :version))))))
         (chatgpt-shell-openai-make-model
+         :version "o1"
+         :token-width 3
+         ;; https://platform.openai.com/docs/models/o1
+         :context-window 200000
+         :validate-command #'chatgpt-shell-validate-no-system-prompt)
+        (chatgpt-shell-openai-make-model
          :version "o1-preview"
          :token-width 3
          ;; https://platform.openai.com/docs/models/gpt-01
