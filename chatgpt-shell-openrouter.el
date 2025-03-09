1707c52e0068888ccd1b2a3bce1fa583a0212fb6
David J. Rosenbaum <djr7c4@gmail.com>
2025-03-09 16:48:08 -0700

    Fix broken token width for anthropic/claude-3.7-sonnet and cleanup the code

--- chatgpt-shell-openrouter.el
+++ chatgpt-shell-openrouter.el
@@ -115,14 +115,13 @@ VALIDATE-COMMAND and OTHER-PARAMS for `chatgpt-shell-openai-make-model'."
          ;;
          ;; See https://openrouter.ai/qwen/qwen-2.5-coder-32b-instruct
          :other-params '((provider (quantizations . ["bf16"]))))
-	(chatgpt-shell-openrouter-make-model
+        (chatgpt-shell-openrouter-make-model
          :version "anthropic/claude-3.7-sonnet"
          :short-version "claude-3.7-sonnet"
-         :label "Claude3.7Sonnet"
-         :token-width 16
-         ;; See
-         :context-window 200000
-	)))
+         :label "Claude"
+         :token-width 4
+         ;; See https://openrouter.ai/anthropic/claude-3.7-sonnet
+         :context-window 200000)))
 
 (defcustom chatgpt-shell-openrouter-api-url-base "https://openrouter.ai/api"
   "OpenRouter API's base URL.
