5a2788d62d6bbe0cf79b78490df3ea83a5c0f3c7
David J. Rosenbaum <djr7c4@gmail.com>
2025-04-15 11:26:13 -0700

    Add Gemini 2.5 pro preview

--- chatgpt-shell-google.el
+++ chatgpt-shell-google.el
@@ -221,7 +221,12 @@ This gets set once for each MODEL, based on a heuristic."
   "Build a list of Google LLM models available."
   ;; Context windows have been verified as of 11/26/2024. See
   ;; https://ai.google.dev/gemini-api/docs/models/gemini.
-  (list (chatgpt-shell-google-make-model :version "gemini-2.5-pro-exp"
+  (list (chatgpt-shell-google-make-model :version "gemini-2.5-pro-preview"
+                                         :short-version "2.5-pro-preview"
+                                         :path "/v1beta/models/gemini-2.5-pro-preview-03-25"
+                                         :token-width 4
+                                         :context-window 1048576)
+        (chatgpt-shell-google-make-model :version "gemini-2.5-pro-exp"
                                          :short-version "2.5-pro-exp"
                                          :path "/v1beta/models/gemini-2.5-pro-exp-03-25"
                                          :token-width 4
